4c69c6573fcce390eb77396031870734198a131e
liyu <lixiaoshenxian@gmail.com>
2015-03-11 19:22:46 +0800

    jekyll

---
+++ res/js/js-ku.js
@@ -0,0 +1,10 @@
+var _hmt = _hmt || [];
+(function() {
+ 
+
+    $("pre").addClass("prettyprint linenums");
+    var pre = document.createElement("script");
+    pre.src = "/res/google-code-prettify/run_prettify.js?lang-js&skin=tomorrow-night";
+    var pre_s = document.getElementsByTagName("script")[0]; 
+    pre_s.parentNode.insertBefore(pre,pre_s);
+})();
